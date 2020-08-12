a136d72eff2ccf02de5993510e17ce1108906d30
Hoàng Phú <63839574+PhuNguyen01997@users.noreply.github.com>
2020-08-12 21:56:29 +0700

    Add files via upload

---
+++ precache-manifest.b33284d5b4e5fb4169dcd98a96c35556.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd98a5f903a2305a6fbea6136c77ebe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8198d97cf99c02d39d16",
+    "url": "/static/css/main.cf31c66e.chunk.css"
+  },
+  {
+    "revision": "561a4f8412dd5237a73f",
+    "url": "/static/js/2.68b90936.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.68b90936.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8198d97cf99c02d39d16",
+    "url": "/static/js/main.4b28f89c.chunk.js"
+  },
+  {
+    "revision": "dd8ecdf8bf2d698e197c",
+    "url": "/static/js/runtime-main.348370cd.js"
+  }
+]);
